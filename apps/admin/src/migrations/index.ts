00436c67896b6615428483c25d1926e0491ee6e8
prashantchataut <prashantchataut8@gmail.com>
2026-07-13 17:43:18 +0545

    fix: remove stale apps/admin directory (renamed to apps/cms) and regenerate lockfile

--- apps/admin/src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20260708_130036 from './20260708_130036';
-
-export const migrations = [
-  {
-    up: migration_20260708_130036.up,
-    down: migration_20260708_130036.down,
-    name: '20260708_130036'
-  },
-];
